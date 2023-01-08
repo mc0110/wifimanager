d511ef08bf14ca80da9fc0a3a1bcb1a3ca7b88ba
Magnus <magnus.christ@gmx.de>
2023-01-08 13:44:48 +0100

    modified:   lib/connect.py 	modified:   lib/web_os.py 	modified:   src/web_os_run.py

--- src/web_os_run.py
+++ src/web_os_run.py
@@ -19,31 +19,33 @@ def run(w):
     # debug=True for debugging
     naw = Nanoweb(80) #, debug = True)
     connect = w
-    os.init(connect)
+    os.init(connect, naw)
 
     naw.STATIC_DIR = "/"
-    naw.routes = {
-         '/': os.index,
-         '/s': os.status,
-         '/loop': os.loop,
-         '/ta': os.toggle_ap,
-         '/ts1': os.set_sta,
-         '/ts': os.toggle_sta,
-         '/rm': os.toggle_run_mode,
-         '/wc': os.creds,
-         '/scan':os.scan_networks,
-         '/cp': os.cp,
-         '/ur': os.ur,
-         '/ur1': os.ur1,
-         '/dc': os.del_cred,
-         '/sc': os.swp_cred,
-         '/rc': os.res_cred,
-         '/rb': os.s_reboot,
-         '/rb1': os.h_reboot,
-         '/upload/*': os.upload,
-         '/fm*': os.fm,
-         '/dir*': os.set_dir,
-     }
+
+
+#     naw.routes = {
+#          '/': os.index,
+#          '/s': os.status,
+#          '/loop': os.loop,
+#          '/ta': os.toggle_ap,
+#          '/ts1': os.set_sta,
+#          '/ts': os.toggle_sta,
+#          '/rm': os.toggle_run_mode,
+#          '/wc': os.creds,
+#          '/scan':os.scan_networks,
+#          '/cp': os.cp,
+#          '/ur': os.ur,
+#          '/ur1': os.ur1,
+#          '/dc': os.del_cred,
+#          '/sc': os.swp_cred,
+#          '/rc': os.res_cred,
+#          '/rb': os.s_reboot,
+#          '/rb1': os.h_reboot,
+#          '/upload/*': os.upload,
+#          '/fm*': os.fm,
+#          '/dir*': os.set_dir,
+#      }
 
     loop = asyncio.get_event_loop()
     loop.create_task(naw.run())
